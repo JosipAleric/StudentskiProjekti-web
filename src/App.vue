6177bda614b44290f98e0e427a5f44a34576f4c6
Josip Aleric <jaleric8@gmail.com>
2023-05-09 19:01:58 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar color="white" dark height="110" flat hide-on-scroll>
+      <v-toolbar-title class="ml-5">
+        <v-img contain src="./assets/logo.png" transition="scale-transition" height="80" />
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items id="nav" class="hidden-sm-and-down">
+        <div>
+          <router-link to="/">Naslovnica</router-link>
+          <router-link to="/about">Projekti</router-link>
+        </div>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+
+      <v-btn color="primary" rounded class="text-capitalize hidden-sm-and-down" height="40" width="150">Prijavi se</v-btn>
+      <v-btn icon @click="drawer = !drawer" large class="mr-5">
+        <v-app-bar-nav-icon color="primary" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" absolute left app temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  data: () => ({
+    //
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Poppins", sans-serif;
+}
+#nav {
+  display: flex;
+  align-items: center;
+}
+
+#nav a {
+  font-weight: 500;
+  color: #094776;
+  text-decoration: none;
+  margin-left: 15px;
+}
+
+#nav a.router-link-exact-active {
+  background-color: #d8d8d8;
+  border-radius: 30px;
+  padding: 10px 20px 10px 20px;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters style="height: 91vh">
+        <v-col cols="12" lg="6" md="12" sm="12" xs="12" class="d-flex align-center justify-center" >
+          <div>
+            <v-divider class="my-8"></v-divider>
+            <h1 class="display-2 font-weight-bold mb-3 text-center" style="color: #094776">SUM Studentski Projekti</h1>
+            <p class="text-h6 text--secondary font-weight-regular text-center" style="max-width: 500px;">
+              Dobrodošli na web stranicu Sveučilišta u Mostaru. <br />
+              Na ovom dijelu sustava možete pronaći detaljne informacije o studentskim projektima.
+            </p>
+            <div class="text-center">
+              <v-btn class="text-capitalize font-weight-medium" depressed color="primary" elevation="24" rounded height="50" width="150">Saznaj više</v-btn>
+            </div>
+            <v-divider class="mt-8"></v-divider>
+          </div>
+        </v-col>
+        <v-col cols="12" lg="6" md="12" sm="12" xs="12" class="d-flex align-center justify-center">
+          <v-img :aspect-ratio="16/9" id="hero_image"
+         src="https://img.freepik.com/free-vector/managers-workshop-training-manager-skills-brainstorming-board_335657-3305.jpg?w=1380&t=st=1683583827~exp=1683584427~hmac=f5c0a0f45511aed53fd032eeb529c4cc39b313d19f853d6ca4f72bf701b576ff" alt=""/>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-row style="height: 400px; margin-top: 100px; background-color: #094776; width: 100vw;" >
+      <v-col cols="12" lg="12">
+        <v-img contain src="../assets/images/landing/Rectangle.svg" transition="scale-transition" height="170" />
+        <v-img contain src="../assets/images/landing/img.svg" transition="scale-transition" height="80" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<style scoped>
+@media screen and (max-width: 1263px) {
+  #hero_image{
+    max-width: 600px;
+  }
+}
+</style>
